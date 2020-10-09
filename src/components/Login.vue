635fb53bf27a8e1970d76b37b38bb5031c765836
FlashNight515 <2391748033@qq.com>
2020-10-09 15:30:45 +0800

    完成了登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 头像区域 -->
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 表单区域 -->
+      <el-form
+        :model="loginForm"
+        ref="loginForm"
+        :rules="LoginFormRules"
+        label-width="0px"
+        class="login-form"
+      >
+        <!-- 用户 -->
+        <el-form-item prop="usernameRule">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-yonghu"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="passwordRule">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-mima"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="submitForm('loginForm')"
+            >登录</el-button
+          >
+          <el-button type="info" @click="resetForm('loginForm')"
+            >重置</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 表单内容数据绑定
+      loginForm: {
+        username: '',
+        password: '',
+      },
+      // 内容验证
+      LoginFormRules: {
+        usernameRule: [
+          { required: true, message: '请输入用户账号', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur',
+          },
+        ],
+        passwordRule: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 12,
+            message: '长度在 6 到 12 个字符',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(async (valid) => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    },
+    resetForm(formName) {
+      // this.$refs[formName].resetFields()
+      this.$router.push('/home')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar-box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login-form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+// 导入图标样式
+import './assets/font/iconfont.css'
 
+import axios from 'axios'
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/appi/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
