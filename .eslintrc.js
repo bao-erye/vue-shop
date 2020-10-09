daf391ae2867e5e43b6b65cde9e5ff80f2005a69
FlashNight515 <2391748033@qq.com>
2020-10-09 15:45:56 +0800

    完成登录功能，调试了分支差错

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'indent':['off',2],
-    'Missing space before function parentheses':0
+    'space-before-function-paren':0
   }
 }
